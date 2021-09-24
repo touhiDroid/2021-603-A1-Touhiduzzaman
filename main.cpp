57e24f39310813b9f01bcad8a76f6ce742b9bc07
touhiDroid <tz08128@gmail.com>
2021-09-23 23:37:56 -0400

    mpi halfdone

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 #include <bits/stdc++.h>
 #include <omp.h>
 #include <pthread.h>
+#include <mpi.h>
 
 #define SEQUENTIAL 1
 #define THREADED 2
@@ -170,6 +171,85 @@ int *KNNOpenMP(ArffData *train, ArffData *test, int k, int num_desired_threads) 
 }
 
 
+int *KNN_MPI(int argc, char *argv[], ArffData *train, ArffData *test, int k) {
+    int ntasks, rank;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &ntasks);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    // predictions is the array where you have to return the class predicted (integer) for the test dataset instances
+    int *predictions = (int *) malloc(test->num_instances() * sizeof(int));
+
+    int num_classes = train->num_classes();
+
+    int n = test->num_instances();
+    // int partition_size = (n + num_desired_threads - 1) / num_desired_threads;
+
+    int start = rank * n / ntasks;
+    int end = (rank + 1) * n / ntasks;
+    if (rank == ntasks - 1)
+        end = n;
+
+    for (int queryIndex = start; queryIndex < end; queryIndex++) {
+
+        // stores k-NN candidates for a query vector as a sorted 2d array. First element is inner product, second is class.
+        float *candidates = (float *) calloc(k * 2, sizeof(float));
+        for (int i = 0; i < 2 * k; i++) { candidates[i] = FLT_MAX; }
+
+        // Stores bincounts of each class over the final set of candidate NN
+        int *classCounts = (int *) calloc(num_classes, sizeof(int));
+
+        for (int keyIndex = 0; keyIndex < train->num_instances(); keyIndex++) {
+
+            float dist = distance(test->get_instance(queryIndex), train->get_instance(keyIndex));
+
+            // Add to our candidates
+            for (int c = 0; c < k; c++) {
+                if (dist < candidates[2 * c]) {
+                    // Found a new candidate
+                    // Shift previous candidates down by one
+                    for (int x = k - 2; x >= c; x--) {
+                        candidates[2 * x + 2] = candidates[2 * x];
+                        candidates[2 * x + 3] = candidates[2 * x + 1];
+                    }
+
+                    // Set key vector as potential k NN
+                    candidates[2 * c] = dist;
+                    candidates[2 * c + 1] = train->get_instance(keyIndex)->get(
+                            train->num_attributes() - 1)->operator float(); // class value
+
+                    break;
+                }
+            }
+        }
+
+        // Bincount the candidate labels and pick the most common
+        for (int i = 0; i < k; i++) {
+            classCounts[(int) candidates[2 * i + 1]] += 1;
+        }
+
+        int max = -1;
+        int max_index = 0;
+        for (int i = 0; i < num_classes; i++) {
+            if (classCounts[i] > max) {
+                max = classCounts[i];
+                max_index = i;
+            }
+        }
+
+        predictions[queryIndex] = max_index;
+
+        for (int i = 0; i < 2 * k; i++) { candidates[i] = FLT_MAX; }
+        memset(classCounts, 0, num_classes * sizeof(int));
+    }
+
+    int *global_predictions = (int *) malloc(test->num_instances() * sizeof(int));
+    MPI_Reduce(&predictions, &global_predictions, n, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    MPI_Finalize();
+    return global_predictions;
+}
+
 void *KnnRunSingleThread(void *arguments) {
     struct ThreadArgs *argus = (struct ThreadArgs *) arguments;
     int k = argus->k;
@@ -243,7 +323,6 @@ int *KNNThreaded(ArffData *train, ArffData *test, int k, int num_desired_threads
     // int partition_size = (n + num_desired_threads - 1) / num_desired_threads;
     pthread_t *threads = (pthread_t *) malloc(num_desired_threads * sizeof(pthread_t));
 
-    // running 1 thread short to let the last one collect all the remaining data points
     for (int t = 0; t < num_desired_threads; t++) {
         int start = t * n / num_desired_threads;
         int end = (t + 1) * n / num_desired_threads;
@@ -331,10 +410,10 @@ int main(int argc, char *argv[]) {
                "Accuracy was %.4f\n", k, test->num_instances(), train->num_instances(),
                (long long unsigned int) diff, accuracy);
     }
-        // endregion : Sequential Version
+    // endregion : Sequential Version
 
 
-        // region : OpenMP version
+    // region : OpenMP version
     else if (version == OPENMP) {
         clock_gettime(CLOCK_MONOTONIC_RAW, &start);
         predictions = KNNOpenMP(train, test, k, num_desired_threads);
@@ -348,10 +427,10 @@ int main(int argc, char *argv[]) {
                "Accuracy was %.4f\n", k, test->num_instances(), train->num_instances(),
                (long long unsigned int) diff, accuracy);
     }
-        // endregion : MPI Version
+    // endregion : OpenMP Version
 
 
-        // region : Threaded Version
+    // region : Threaded Version
     else if (version == THREADED) {
         clock_gettime(CLOCK_MONOTONIC_RAW, &start);
         predictions = KNNThreaded(train, test, k, num_desired_threads);
@@ -364,14 +443,35 @@ int main(int argc, char *argv[]) {
         printf("The %i-NN classifier for %lu test instances on %lu train instances required %llu ms CPU time. "
                "Accuracy was %.4f\n", k, test->num_instances(), train->num_instances(),
                (long long unsigned int) diff, accuracy);
-    } else if (version == MPI) {
+    }
+    // endregion : Threaded Version
+
+    // region : MPI Version
+    else if (version == MPI) {
         // Still a TODO MPI Version
-    } else {
-        cout << "Invalid Version!\nVERSION ->\n\t1 for Sequential,\n\t2 for Threaded,\n\t3 for OpenMP,\n\t4 for MPI"
+        clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+        predictions = KNN_MPI(argc, argv, train, test, k, num_desired_threads);
+        clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+        // Compute the confusion matrix
+        int *confusionMatrix = computeConfusionMatrix(predictions, test);
+        // Calculate the accuracy
+        float accuracy = computeAccuracy(confusionMatrix, test);
+        uint64_t diff = (1000000000L * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec) / 1e6;
+        printf("%i-NN  -  %lu test  - %lu train  -  %llu ms  -   %.4f%%\\n", k,
+               test->num_instances(), train->num_instances(),
+               (long long unsigned int) diff, accuracy));
+        /*printf("The %i-NN classifier for %lu test instances on %lu train instances required %llu ms CPU time. "
+               "Accuracy was %.4f\n", k, test->num_instances(), train->num_instances(),
+               (long long unsigned int) diff, accuracy);*/
+    }
+    // endregion : MPI Version
+
+    else {
+        cout << "Invalid Version Selection!"
+                "\nVERSION ->\n\t1 for Sequential,\n\t2 for Threaded,\n\t3 for OpenMP,\n\t4 for MPI"
              << endl;
         exit(0);
     }
 
-    // endregion : Threaded Version
-
+    free(predictions);
 }
